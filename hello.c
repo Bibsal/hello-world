809e5487f1e85c1ca9065d9beace2a2be56e8c45
Bibsal <55621344+Bibsal@users.noreply.github.com>
2020-09-08 08:19:09 -0700

    Update and rename README.md to hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+# hello-world
+A try for repository
+
+#include<stdio.h>
+void main()
+{
+printf("Hiiiiii Legends");
+
+printf("My Name is Bishal , and ive just landed up to this Github platform ,
+So that we can help each other's in coding or designing kinda problems !");
+
+
+printf("So that's all for now , later ill be uploading my codes , SEE YA LOVELY PEOPLE....");
+getch();
+}
+
+
